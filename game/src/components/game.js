e6d49f6f11329647c8806461da796246c906cb95
BrandyBecker <brandybeckerfsw@gmail.com>
2020-06-24 12:47:02 -0400

    creates nav bar and routes to correct components

---
+++ game/src/components/game.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Game() {
+  return (
+    <div className="home">
+      <p>Game</p>
+    </div>
+  );
+}
+
+export default Game;
---
+++ game/src/components/nav/nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './nav.css';
+import {Link} from 'react-router-dom';
+
+function Nav() {
+  return (
+    <div className="nav">
+      <h3>🌟 Adventure Game 🌟</h3>
+      <div class="navlinks">
+        <Link to="/">Home</Link>
+        <Link to="/game">Game</Link>
+        <Link to="/faq">FAQ</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
